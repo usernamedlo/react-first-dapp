41f996055171df510404c34d0b6f9ee3425daebd
Loïc Ghijselings <l.ghijselings@student.helmo.be>
2023-02-12 14:52:39 +0100

    first dapp done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 function App() {
+  const [greeting, setGreetingValue] = useState();
+
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
+
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+      try {
+        const data = await contract.greet();
+        setGreetingValue(data);
+        console.log('data: ', data);
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+  }
+
+  async function setGreeting() {
+    if (!greeting) return;
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount();
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue('');
+      await transaction.wait();
+      fetchGreeting();
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{greeting}</p>
+     <input onChange={e => setGreetingValue(e.target.value)} placeholder="Set greeting" />
+     <button onClick={setGreeting}>Change</button>
     </div>
   );
-}
+  }
 
 export default App;
